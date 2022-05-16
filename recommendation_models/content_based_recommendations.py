0828bc7b1560e9de69adcc8402d835eb598142f5
kparde <35860479+kparde@users.noreply.github.com>
2022-05-17 04:11:20 +0530

    Update content_based_recommendations.py

--- recommendation_models/content_based_recommendations.py
+++ recommendation_models/content_based_recommendations.py
@@ -47,7 +47,7 @@ import time
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import sklearn
-import fastparquet
+from setuptools import fastparquet
 import scipy
 
 
