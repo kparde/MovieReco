f4564fd110f85e80dd83ae1ca1db15d90b496bbb
kparde <35860479+kparde@users.noreply.github.com>
2022-05-17 04:17:46 +0530

    Update personalized_rec_app.py

--- pages/personalized_rec_app.py
+++ pages/personalized_rec_app.py
@@ -39,7 +39,7 @@ import random
 import time
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from setuptools import fastparquet
+import setuptools 
 import streamlit as st
 import pickle
 import scipy
