aabb3f3e2e25a97a01690bf3024654dc5f728ecf
kparde <35860479+kparde@users.noreply.github.com>
2022-05-17 04:10:25 +0530

    Update collab_recommendations.py

--- recommendation_models/collab_recommendations.py
+++ recommendation_models/collab_recommendations.py
@@ -35,7 +35,7 @@ import random
 import time
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-import fastparquet
+from setuptools import fastparquet
 import pickle
 import scipy
 import sklearn
